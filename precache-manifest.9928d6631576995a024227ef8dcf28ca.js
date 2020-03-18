00b398e8c21f4e55ca010cd3a71c2d653c661af9
kcognata <kcognata@lyonscg.com>
2020-03-18 11:50:14 -0400

    Updates

---
+++ precache-manifest.9928d6631576995a024227ef8dcf28ca.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1c4ba9d1eae719a6c478d7a750efb37",
+    "url": "/react-pokemon-list/index.html"
+  },
+  {
+    "revision": "4ddf30e8d8c0d8661c35",
+    "url": "/react-pokemon-list/static/css/main.7ae77ed1.chunk.css"
+  },
+  {
+    "revision": "248d37739ddfd2d0b03b",
+    "url": "/react-pokemon-list/static/js/2.f7a68270.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-pokemon-list/static/js/2.f7a68270.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ddf30e8d8c0d8661c35",
+    "url": "/react-pokemon-list/static/js/main.65a2e1fb.chunk.js"
+  },
+  {
+    "revision": "8c6bb1afcf09260f15f3",
+    "url": "/react-pokemon-list/static/js/runtime-main.3aa656c6.js"
+  },
+  {
+    "revision": "f019b28580b874866cffb10198ce25a7",
+    "url": "/react-pokemon-list/static/media/cardback.f019b285.png"
+  },
+  {
+    "revision": "9058a2301f70aef255a27591eb59d370",
+    "url": "/react-pokemon-list/static/media/fire-bg.9058a230.png"
+  },
+  {
+    "revision": "74de8bff8cc247c7149310960639227d",
+    "url": "/react-pokemon-list/static/media/flying-bg.74de8bff.jpg"
+  },
+  {
+    "revision": "20fe1e143ea1bb175a2035b1d180e398",
+    "url": "/react-pokemon-list/static/media/grass-bg.20fe1e14.jpg"
+  },
+  {
+    "revision": "7c575b2898a794162369cb1db1ad2c36",
+    "url": "/react-pokemon-list/static/media/normal-bg.7c575b28.png"
+  },
+  {
+    "revision": "b88d7dd8135e809d505fccd5d0573756",
+    "url": "/react-pokemon-list/static/media/types-key.b88d7dd8.png"
+  },
+  {
+    "revision": "8123b9245fe07cc0ccb46f0ad925e521",
+    "url": "/react-pokemon-list/static/media/types.8123b924.png"
+  },
+  {
+    "revision": "a50b9b3ebd50e1fbc2f0978b43039a1a",
+    "url": "/react-pokemon-list/static/media/water-bg.a50b9b3e.jpg"
+  }
+]);
